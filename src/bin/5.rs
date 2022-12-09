ffb71f944c4fef115463e22900ae1799b20f32a6
WatDuhHekBro <watduhhekbro@protonmail.com>
2022-12-08 20:00:05 -0600

    Finished Day 5

--- src/bin/5.rs
+++ src/bin/5.rs
@@ -1,23 +1,25 @@
 use lazy_static::lazy_static;
 use regex::Regex;
-use std::collections::HashMap;
 
 fn main() {
     // Part A
-    let (mut crates, instructions, length) = parse_input(INPUT);
+    let (mut crates, instructions) = parse_input(INPUT);
     execute_instructions(&mut crates, &instructions);
-    let output = get_top_chars(&mut crates, length);
+    let output = get_top_chars(&mut crates);
     println!("Part A: {output}");
 
     // Part B
-    println!("Part B: ");
+    let (mut crates, instructions) = parse_input(INPUT);
+    execute_upgraded_instructions(&mut crates, &instructions);
+    let output = get_top_chars(&mut crates);
+    println!("Part B: {output}");
 }
 
 #[derive(Debug, PartialEq)]
 struct Instruction {
     amount: i32,
-    src_index: i32,
-    dest_index: i32,
+    src_index: usize,
+    dest_index: usize,
 }
 
 // This enum will be used to keep track of which parsing mode is currently being used
@@ -26,13 +28,26 @@ enum ParserMode {
     Instructions,
 }
 
+// This trait is implemented because the amount of columns present isn't initially known.
+// If you want to access the vector at index 5, you first need to create all the vectors up to index 5.
+trait Expandable {
+    fn populate(&mut self, new_length: usize);
+}
+
+impl<T> Expandable for Vec<Vec<T>> {
+    fn populate(&mut self, new_length: usize) {
+        // Start from the current length and loop to the index needed to push the sub Vec
+        for _ in self.len()..=new_length {
+            self.push(Vec::new());
+        }
+    }
+}
+
 // The parser will return two items: A map of stacks (no native Stack in std) for each column, and a list of instructions
-// If a Vec was used to hold the stacks, you'd have to implement extra logic to create all the Vecs before (if the first crate found started at index 5 for example)
 // Each "stack" should start at the bottom. Due to the nature of the input however, each Vec will need to be reversed at the end.
-fn parse_input(input: &str) -> (HashMap<i32, Vec<char>>, Vec<Instruction>, i32) {
-    let mut crates: HashMap<i32, Vec<char>> = HashMap::new();
+fn parse_input(input: &str) -> (Vec<Vec<char>>, Vec<Instruction>) {
+    let mut crates: Vec<Vec<char>> = Vec::new();
     let mut instructions: Vec<Instruction> = Vec::new();
-    let mut length = 0;
     let mut mode = ParserMode::Crates;
 
     for line in input.split('\n') {
@@ -57,13 +72,12 @@ fn parse_input(input: &str) -> (HashMap<i32, Vec<char>>, Vec<Instruction>, i32) 
                         // Parse the character and push it onto the target stack if there's a crate
                         // Otherwise, if no crate exists at this triplet, move on
                         else if a == '[' && c == ']' {
-                            // HashMap::insert will overwrite, so check if something already exists there
-                            if !crates.contains_key(&index) {
-                                crates.insert(index, Vec::new());
-                            }
+                            // The inner index will be 0-indexed as the crate index is 1-indexed.
+                            let index = index - 1;
+                            crates.populate(index);
 
                             let stack = crates
-                                .get_mut(&index)
+                                .get_mut(index)
                                 .expect("Index should already be populated");
 
                             stack.push(b);
@@ -76,11 +90,6 @@ fn parse_input(input: &str) -> (HashMap<i32, Vec<char>>, Vec<Instruction>, i32) 
                             index += 1;
                         }
                     }
-
-                    // Take the highest index reached of each row
-                    if length < index {
-                        length = index;
-                    }
                 }
                 ParserMode::Instructions => {
                     lazy_static! {
@@ -92,22 +101,22 @@ fn parse_input(input: &str) -> (HashMap<i32, Vec<char>>, Vec<Instruction>, i32) 
 
                     instructions.push(Instruction {
                         amount: (&captures[1]).parse::<i32>().unwrap().to_owned(),
-                        src_index: (&captures[2]).parse::<i32>().unwrap().to_owned(),
-                        dest_index: (&captures[3]).parse::<i32>().unwrap().to_owned(),
+                        src_index: (&captures[2]).parse::<usize>().unwrap().to_owned(),
+                        dest_index: (&captures[3]).parse::<usize>().unwrap().to_owned(),
                     });
                 }
             }
         }
     }
 
-    for (_, stack) in crates.iter_mut() {
+    for stack in crates.iter_mut() {
         stack.reverse();
     }
 
-    (crates, instructions, length)
+    (crates, instructions)
 }
 
-fn execute_instructions(crates: &mut HashMap<i32, Vec<char>>, instructions: &Vec<Instruction>) {
+fn execute_instructions(crates: &mut Vec<Vec<char>>, instructions: &Vec<Instruction>) {
     for Instruction {
         amount,
         src_index,
@@ -118,7 +127,7 @@ fn execute_instructions(crates: &mut HashMap<i32, Vec<char>>, instructions: &Vec
             // Due to the fact that only one mutable reference is allowed to exist at a time, this ugliness must ensue
             let token = {
                 let src_stack = crates
-                    .get_mut(src_index)
+                    .get_mut(*src_index - 1)
                     .expect("Invalid instruction index or data setup");
                 src_stack.pop()
             };
@@ -126,7 +135,7 @@ fn execute_instructions(crates: &mut HashMap<i32, Vec<char>>, instructions: &Vec
             // Do nothing if the stack is empty
             if let Some(token) = token {
                 let dest_stack = crates
-                    .get_mut(dest_index)
+                    .get_mut(*dest_index - 1)
                     .expect("Invalid instruction index or data setup");
                 dest_stack.push(token);
             }
@@ -134,58 +143,62 @@ fn execute_instructions(crates: &mut HashMap<i32, Vec<char>>, instructions: &Vec
     }
 }
 
-fn get_top_chars(crates: &mut HashMap<i32, Vec<char>>, length: i32) -> String {
+fn get_top_chars(crates: &mut Vec<Vec<char>>) -> String {
     let mut output = String::new();
 
-    for index in 1..=length {
-        let stack = crates.get_mut(&index).expect("HashMap not setup properly");
-        stack.reverse();
-
+    for stack in crates.iter_mut() {
+        // Columns can be empty
         if let Some(token) = stack.pop() {
             output.push(token);
         }
     }
 
     output
-
-    /*let keys = crates.keys();
-    // Because HashMaps don't ever give out a proper order of their keys, this requires the janky solution of sorting the keys then using them again.
-    let mut ordered_keys = Vec::new();
-
-    for key in keys {
-        ordered_keys.push(key);
-    }
-
-    ordered_keys.sort_unstable();
-
-    for key in ordered_keys {
-        crates.get_mut(key).unwrap();
-    }*/
 }
-
-////////////
-// Part A //
-////////////
 
 ////////////
 // Part B //
 ////////////
 
+fn execute_upgraded_instructions(crates: &mut Vec<Vec<char>>, instructions: &Vec<Instruction>) {
+    for Instruction {
+        amount,
+        src_index,
+        dest_index,
+    } in instructions
+    {
+        let mut section: Vec<char> = {
+            let src_stack = crates
+                .get_mut(*src_index - 1)
+                .expect("Invalid instruction index or data setup");
+
+            // Slice off the ending chunk
+            // If the length is 4 (for [A, B, C, D]) and the amount you want to transfer is 3, then drain "1.."
+            src_stack
+                .drain(src_stack.len() - *amount as usize..)
+                .collect()
+        };
+
+        let dest_stack = crates
+            .get_mut(*dest_index - 1)
+            .expect("Invalid instruction index or data setup");
+
+        // Then append that ending chunk to the destination
+        dest_stack.append(&mut section);
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::{execute_instructions, get_top_chars, parse_input, Instruction};
-    use std::collections::HashMap;
+    use crate::{
+        execute_instructions, execute_upgraded_instructions, get_top_chars, parse_input,
+        Instruction,
+    };
 
     #[test]
     fn sample_parser_works() {
-        let (crates, instructions, length) = parse_input(SAMPLE_INPUT);
-
-        let mut crates2 = HashMap::new();
-        crates2.insert(1, vec!['Z', 'N']);
-        crates2.insert(2, vec!['M', 'C', 'D']);
-        crates2.insert(3, vec!['P']);
-
-        assert_eq!(length, 3);
+        let (crates, instructions) = parse_input(SAMPLE_INPUT);
+        let crates2 = vec![vec!['Z', 'N'], vec!['M', 'C', 'D'], vec!['P']];
 
         assert_eq!(crates, crates2);
         assert_eq!(
@@ -217,54 +230,42 @@ mod tests {
 
     #[test]
     fn execute_instructions_works() {
-        let (mut crates, instructions, _) = parse_input(SAMPLE_INPUT);
+        let (mut crates, instructions) = parse_input(SAMPLE_INPUT);
         execute_instructions(&mut crates, &instructions);
-
-        let mut crates2 = HashMap::new();
-        crates2.insert(1, vec!['C']);
-        crates2.insert(2, vec!['M']);
-        crates2.insert(3, vec!['P', 'D', 'N', 'Z']);
-
+        let crates2 = vec![vec!['C'], vec!['M'], vec!['P', 'D', 'N', 'Z']];
         assert_eq!(crates, crates2);
     }
 
-    // Currently failing test for arcane reasons, need to investigate later
+    #[test]
+    fn execute_upgraded_instructions_works() {
+        let (mut crates, instructions) = parse_input(SAMPLE_INPUT);
+        execute_upgraded_instructions(&mut crates, &instructions);
+        let crates2 = vec![vec!['M'], vec!['C'], vec!['P', 'Z', 'N', 'D']];
+        assert_eq!(crates, crates2);
+    }
+
     #[test]
     fn get_top_chars_works() {
-        let mut crates = HashMap::new();
-        crates.insert(1, vec!['A', 'B']);
-        crates.insert(2, vec!['X', 'Y', 'Z']);
-
-        let asdf = crates.get_mut(&2).unwrap();
-        asdf.reverse();
-
-        // Reads ['A', 'B'] then ['X', 'Y'] only in a for loop, really weird, need to look into this
-        for index in 1..=2 {
-            let stack = crates.get_mut(&index).expect("HashMap not setup properly");
-            let a = stack.len();
-            let b = a + 1;
-            println!("{b}");
-            stack.reverse();
-
-            if let Some(token) = stack.pop() {
-                print!("{token}");
-            }
-        }
-
-        let output = get_top_chars(&mut crates, 3);
-        assert_eq!(output, "CMZ");
+        let mut crates = vec![vec!['A', 'B'], vec!['C', 'D', 'E']];
+        let output = get_top_chars(&mut crates);
+        assert_eq!(output, "BE");
     }
 
     #[test]
     fn sample_runner_a() {
-        let (mut crates, instructions, length) = parse_input(SAMPLE_INPUT);
+        let (mut crates, instructions) = parse_input(SAMPLE_INPUT);
         execute_instructions(&mut crates, &instructions);
-        let output = get_top_chars(&mut crates, length);
+        let output = get_top_chars(&mut crates);
         assert_eq!(output, "CMZ");
     }
 
     #[test]
-    fn sample_runner_b() {}
+    fn sample_runner_b() {
+        let (mut crates, instructions) = parse_input(SAMPLE_INPUT);
+        execute_upgraded_instructions(&mut crates, &instructions);
+        let output = get_top_chars(&mut crates);
+        assert_eq!(output, "MCD");
+    }
 
     const SAMPLE_INPUT: &str = "    [D]
 [N] [C]
